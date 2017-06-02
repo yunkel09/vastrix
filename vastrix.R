88b51c6f28e9e0d0831c3f4bb5ac35aed6c6c8f1
William Chavarria <wchavarria@tigo.com.gt>
2017-06-02 13:08:59 -0600

    second commit

--- vastrix.R
+++ vastrix.R
@@ -1,21 +1,36 @@
 #   ____________________________________________________________________________
-#   Nombre del proyecto                                                     ####
+#   vastrix                                                                ####
 
 
-##  ............................................................................
-##  Configure options and load functions                                    ####
+#  Required Packages                                                       
 
-library(dplyr)
-library(tibble)
+    library(dplyr)
+    library(tibble)
 
+# 1. Cargar el data frame                                                           
 
-##  ............................................................................
-##  Step 1: Load Dataset                                                    ####
+    url_vastrix <- 'http://talent.com.gt/IntroR/Trx_Vastrix.rda'
+    download.file(url = url_vastrix, destfile = 'vastrix.rda')
+    load('vastrix.rda')
+    
+    vastrix <- as_tibble(Trx_Vastrix)
+    rm(Trx_Vastrix)
+    
 
-url_vastrix <- 'http://talent.com.gt/IntroR/Trx_Vastrix.rda'
-download.file(url = url_vastrix, destfile = 'vastrix.rda')
-load('vastrix.rda')
+# 2. Determinar cantidad de registros
 
-vastrix <- as_tibble(Trx_Vastrix)
-rm(Trx_Vastrix)
-
+    n <- nrow(vastrix)
+    
+# 3. Resumen Diario
+    
+    Resumen_Diario <- vastrix %>% group_by(FCT_DT) %>% summarise(
+                                                            no_transacciones = n(),
+                                                            valor_total = sum(BILLD_AMNT),
+                                                            valor_promedio = mean(BILLD_AMNT),
+                                                            no_usarios_unicos = n_distinct(AR_KEY),
+                                                            no_transacciones_electronicas = sum(VSTRX_CLSS == 'EPIN'),
+                                                            no_transacciones_fisicas = sum(VSTRX_CLSS == 'PIN')
+                                                        )
+    
+# 4.   
+    
